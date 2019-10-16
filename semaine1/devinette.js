c5b54cb5260dde2151bb9a8464abf19097c20763
TonySeperoumal <tony.seperoumal@gmail.com>
2019-10-16 13:40:33 +0200

    exo 1.1.1

--- semaine1/devinette.js
+++ semaine1/devinette.js
@@ -5,16 +5,29 @@ const rli = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-let solution = 0
+let solution = Math.floor(Math.random()*100+1)
 rli.on('line', (userinput) => {
     console.log("Vous avez entré:", userinput)
-    if(userinput == solution){
+    console .log(solution)
+    if(Math.abs(userinput - solution) <= 5 && userinput != solution){
+        console.log("Vous êtes trés proche, réessayer !")
+    }
+    else if (Math.abs(userinput - solution) <= 10 && userinput != solution){
+        console.log("Vous êtes proche, réessayer !")
+    }
+    else if (Math.abs(userinput - solution) > 20 && userinput != solution){
+        console.log("Vous êtes loin, réessayer !")
+    }
+    else if (userinput == solution){
+        console.log("Gagné !")
         rli.close()
     }
+
+    
 })
 
 
 //Navigateur
 
-let solution = Math.floor(Math.random()*100+1)
-let entree = window.prompt("Devinez le nombre entre 1 et 100") 
+//let solution = Math.floor(Math.random()*100+1)
+//let entree = window.prompt("Devinez le nombre entre 1 et 100") 
